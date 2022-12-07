c6501c0e43f68ea58cc42a21be5f058a9e562a88
Tom Cooper <tom.cooper@acm.org>
2022-12-06 22:27:21 -0500

    day 6

---
+++ day6.py
@@ -0,0 +1,17 @@
+# day 6
+from collections import deque
+
+def solve(n):
+    with open('data/day6.txt', 'r') as f:
+        data = f.read().rstrip()
+
+    buffer = deque(maxlen=n)
+    for i in range(len(data)):
+        buffer.append(data[i])
+        if len(set(buffer)) == n:
+            return i + 1
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {solve(4)}")
+    print(f"Part 1: {solve(14)}")
