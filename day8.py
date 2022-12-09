05b1a99b87761931f1df338966b401e01f136e8a
Tom Cooper <tcooper@frontlineed.com>
2022-12-09 08:59:06 -0500

    day 8

--- day8.py
+++ day8.py
@@ -1,55 +1,46 @@
 # day 8
 
 import numpy as np
-from itertools import chain
-from pprint import pprint
 
 def compute_paths(n):
     pass
 
 def build_grid():
-    with open('data/sample.txt') as f:
+    with open('data/day8.txt') as f:
         return np.array([list(map(int, list(line.rstrip())))
             for line in f.readlines()], dtype=int)
 
-def build_visibles(n):
-    v = np.zeros((n, n), dtype=int)
-    for i in range(n):
-        v[i, 0] = v[i, n-1] = v[0, i] = v[n-1, i] = 1
-    return v
+def east(n):
+    for r in range(1, n-1):
+        yield [[r, c] for c in range(0, n-1)]
 
-def taller(h, lst):
-    print(f"Is {h} greater than all of {lst}?")
-    result = all(map(lambda x: h > x, lst))
-    print(result)
-    return result
+def west(n):
+    for r in range(1, n-1):
+        yield [[r, c] for c in range(n-1, 0, -1)]
 
-def visible(grid, r, c):
-    height = grid[r,c]
-    # east, west, north, south
-    taller(height, grid[r,c+1:]) \
-        or taller(height, grid[r,:c-1]) \
-        or taller(height, grid[:r-1,c]) \
-        or taller(height, grid[r+1:,c])
+def north(n):
+    for c in range(1, n-1):
+        yield [[r, c] for r in range(0, n-1)]
+
+def south(n):
+    for c in range(1, n-1):
+        yield [[r, c] for r in range(n-1, 0, -1)] 
 
 def solve():
     grid = build_grid()
     n, _ = grid.shape
-    seen = build_visibles(n)
-    peek_over = lambda p1, p2: grid[*p1] < grid[*p2]
+    seen = np.zeros((n, n), dtype=int)
 
-    # Look in from every direction
-    for r, c in [[r, c] for r in range(1, n-2) for c in range(1, n-2)]:
-        if not seen[r, c] and visible(grid, r, c):
-            seen[r, c] = 1
+    for direction in [east(n), west(n), north(n), south(n)]:
+        for paths in direction:
+            peak = -1
+            for r, c in paths:
+                if grid[r,c] > peak:
+                    seen[r,c] = 1
+                    peak = grid[r,c]
 
-    pprint(grid)
-    pprint(seen)
-    print(np.sum(seen))
+    # Add 4 corners
+    return(np.sum(seen) + 4)
 
 if __name__ == '__main__':
     print(f"Part 1 is {solve()}")
-
-   #  pprint(list(south(5)))
-
-    # 5219 is too high
